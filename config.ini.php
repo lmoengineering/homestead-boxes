755fada9601dadea1dcdad6389b8cf29b9cb7026
Jerry Price <jerryprice@lmo.com>
2016-07-14 10:30:55 -0400

    missing config for phpmyadmin

---
+++ config.ini.php
@@ -0,0 +1,9 @@
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['user'] = 'homestead';
+$cfg['Servers'][1]['password'] = 'secret';
+$cfg['Servers'][1]['host'] = '192.168.10.10';
+
+$cfg['Servers'][2]['auth_type'] = 'config';
+$cfg['Servers'][2]['user'] = 'homestead';
+$cfg['Servers'][2]['password'] = 'secret';
+$cfg['Servers'][2]['host'] = '192.168.10.11';
