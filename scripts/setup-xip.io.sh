be8faf314b31e953febffdd9ec65ee5accebf072
Jerry Price <jerryprice@lmo.com>
2017-03-07 10:15:27 -0500

    change xip.io to wildcard

--- scripts/setup-xip.io.sh
+++ scripts/setup-xip.io.sh
@@ -3,7 +3,8 @@
 LAN_IP=$(/sbin/ifconfig |grep -B1 "inet addr" |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' |awk -F: '{ print $1 ":" $3 }' |awk -F: '{ if ( $1 == "eth2" || $1 == "enp0s9" ) { print $2 } }');
 
 SITE=$1
-ADDR="$1.$LAN_IP.xip.io"
+#ADDR="$1.$LAN_IP.xip.io"
+ADDR="$1.*" # wildcard should be enough to work from all IPs
 
 echo "LAN IP: " $LAN_IP
 echo "ADDR: " $ADDR
